eac0ef3dc99ea93a774b2ce029f0e6250edc2dab
Desiree Castro <desiree.anne.castro@oocl.com>
2022-09-15 11:54:42 +0800

    feat: added a basic layout and clickable routes in the homepage

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import NotFoundPage from './pages/NotFoundPage';
 import HelpPage from './pages/HelpPage';
 import AboutPage from './pages/AboutPage';
+import BasicLayout from './layout/BasicLayout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <TodoList />} />
-        <Route path="/about" element={ <AboutPage />} />
-        <Route path="/help" element={ <HelpPage />} />
-        <Route path="/*" element={ <NotFoundPage />} />
+        <Route path="/" element={<BasicLayout />}>
+          <Route path="/" element={ <TodoList />} />
+          <Route path="about" element={ <AboutPage />} />
+          <Route path="help" element={ <HelpPage />} />
+          <Route path="*" element={ <NotFoundPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
