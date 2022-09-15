3366346ed3216c801f6ab9322a393d1f25423f27
Desiree Castro <desiree.anne.castro@oocl.com>
2022-09-15 17:44:25 +0800

    feat: antd test with layout

--- src/layout/BasicLayout.jsx
+++ src/layout/BasicLayout.jsx
@@ -1,21 +1,32 @@
-import {Footer} from "antd/lib/layout/layout";
-import React from 'react'
+import { Layout, Menu} from 'antd';
 import { Link, Outlet } from 'react-router-dom';
+import React from 'react';
+
+const { Header, Sider, Content, Footer} = Layout;
 
 function BasicLayout(){
     return(
-        <div>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/help">Help</Link>
-                <Link to="/404">404</Link>
-            </nav>
-            <div className="content">
-                <Outlet />
-            </div>
+        <Layout>
+            <Header>To-do List</Header>
+            <Layout>
+            <Sider>
+                <div>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/help">Help</Link>
+                    <Link to="/404">404</Link>
+                </nav>
+                </div>
+            </Sider>
+            <Content>
+                <div className="content">
+                    <Outlet />
+                </div>
+            </Content>
+            </Layout>
             <Footer>Created by Me</Footer>
-        </div>
+        </Layout>
     )
 }
 
